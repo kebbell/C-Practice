8f911afc1d501999caffeff7fe226f3be7c3cb8e
Sam <samuelkebbell@gmail.com>
2024-11-08 14:47:27 +0000

    Updates

--- Program.cs
+++ Program.cs
@@ -72,3 +72,18 @@ int num9 = 6;
 Console.WriteLine(num7 * num8 * num9);
 
 
+// 7. Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing two numbers entered by the user.
+// Test Data:
+// Input the first number: 25
+// Input the second number: 4
+// Expected Output:
+// 25 + 4 = 29
+// 25 - 4 = 21
+// 25 x 4 = 100
+// 25 / 4 = 6
+// 25 mod 4 = 1
+
+int num10 = 25;
+int num11 = 4;
+
+Console.WriteLine();
